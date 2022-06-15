2d3d12136072e8a5dd1691299798bb05b2e1fd33
Erickcufe <erickcufe94@gmail.com>
2022-06-15 17:58:50 -0500

    .

--- R/getCell_Track.R
+++ R/getCell_Track.R
@@ -2,26 +2,38 @@ getCell_Track <- function(celltType="ASC", start = 44883309, end = 44931881,
                           chr = "chr19",
                           perCondition=FALSE,
                           strand = "-"){
-  # library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-  # library(org.Hs.eg.db)
-  # library(Gviz)
   library(rtracklayer)
   library(trackViewer)
+  if(perCondition==FALSE){
+    gr_1 <- GRanges(chr,
+                    IRanges(start = start, end = end),
+                    strand = strand)
 
-  gr_1 <- GRanges(chr,
-                  IRanges(start = start, end = end),
-                          strand = strand)
+    file_cell <- paste0("data_AD_NatGen_2021/celltypes/", celltType, ".bw")
+    Track_celltype <- importScore(file = file_cell,
+                                  ranges= gr_1, format = "BigWig")
+    Track_celltype$dat <- coverageGR(Track_celltype$dat)
 
-  file_cell <- paste0("data_AD_NatGen_2021/celltypes/", celltType, ".bw")
-  Track_celltype <- importScore(file = file_cell,
-                                ranges= gr_1, format = "BigWig")
-  Track_celltype$dat <- coverageGR(Track_celltype$dat)
+    return(Track_celltype)
+  } else {
+    gr_1 <- GRanges(chr,
+                    IRanges(start = start, end = end),
+                    strand = strand)
 
-  optSty <- optimizeStyle(trackList(Track_celltype), theme="safe")
-  trackList <- optSty$tracks
-  viewerStyle <- optSty$style
-  vp <- viewTracks(trackList, gr=gr_1, viewerStyle=viewerStyle,
-                   autoOptimizeStyle=TRUE)
+    file_cell_AD <- paste0("data_AD_NatGen_2021/bigwigs/", celltType, "_AD.bw")
+    file_cell_control <- paste0("data_AD_NatGen_2021/bigwigs/", celltType, "_Control.bw")
 
+    Track_celltype_AD <- importScore(file = file_cell_AD,
+                                  ranges= gr_1, format = "BigWig")
+    Track_celltype_Control <- importScore(file = file_cell_control,
+                                     ranges= gr_1, format = "BigWig")
 
-}
+    Track_celltype_AD$dat <- coverageGR(Track_celltype_AD$dat)
+    Track_celltype_Control$dat <- coverageGR(Track_celltype_Control$dat)
+
+    tracks <- list(AD = Track_celltype_AD,
+                   Control = Track_celltype_Control)
+
+    return(tracks)
+    }
+  }
