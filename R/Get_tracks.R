27e3e1da48756f7ad310df79a819879fa1440ba5
Erickcufe <erickcufe94@gmail.com>
2022-06-15 13:40:00 -0500

    .

--- R/Get_tracks.R
+++ R/Get_tracks.R
@@ -2,5 +2,10 @@ a <- jsonlite::fromJSON("https://api.genome.ucsc.edu/getData/track?hubUrl=https:
 
 b <- jsonlite::fromJSON("https://api.genome.ucsc.edu/getData/track?genome=hg38;track=gold;chrom=chr1;start=47000;end=48000")
 
-"AD_NatGen_2021"
 
+a <- jsonlite::fromJSON("https://api.genome.ucsc.edu/list/publicHubs")
+
+a <- jsonlite::fromJSON("https://api.genome.ucsc.edu/getData/track?hubUrl=https://hgdownload.soe.ucsc.edu/hubs/primates/hub.txt;genome=human")
+
+
+a <- jsonlite::fromJSON("https://api.genome.ucsc.edu/getData/track?genome=hg38;track=gold;chrom=chrM")
